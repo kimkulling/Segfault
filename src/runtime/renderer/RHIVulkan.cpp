64e4dfe6b4937cdb1865ebc853357bfa5bc6b0a9
Kim Kulling <kim.kulling@draeger.com>
2025-07-25 15:53:29 +0200

    Vulkan: Add validation layer support

--- src/runtime/renderer/RHIVulkan.cpp
+++ src/runtime/renderer/RHIVulkan.cpp
@@ -3,11 +3,13 @@
 #include "SDL_vulkan.h"
 
 #include <vector>
+#include <iostream>
 
 namespace segfault::renderer {
     
     struct RHIImpl {
         SDL_Window *window = nullptr;
+        bool enableValidationLayers = false;
         VkInstance instance;
         VkPhysicalDevice physicalDevice;
         VkSurfaceKHR surface;
@@ -30,14 +32,70 @@ namespace segfault::renderer {
         appInfo.apiVersion = VK_API_VERSION_1_0;
     }
 
+    const std::vector<const char*> validationLayers = {
+        "VK_LAYER_KHRONOS_validation"
+    };
+
     bool checkValidationLayerSupport() {
-        uint32_t layerCount;
+        uint32_t layerCount = 0;
         vkEnumerateInstanceLayerProperties(&layerCount, nullptr);
 
         std::vector<VkLayerProperties> availableLayers(layerCount);
         vkEnumerateInstanceLayerProperties(&layerCount, availableLayers.data());
 
-        return false;
+        for (const char* layerName : validationLayers) {
+            bool layerFound = false;
+
+            for (const auto& layerProperties : availableLayers) {
+                if (strcmp(layerName, layerProperties.layerName) == 0) {
+                    layerFound = true;
+                    break;
+                }
+            }
+
+            if (!layerFound) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    static VKAPI_ATTR VkBool32 VKAPI_CALL debugCallback(
+        VkDebugUtilsMessageSeverityFlagBitsEXT messageSeverity,
+        VkDebugUtilsMessageTypeFlagsEXT messageType,
+        const VkDebugUtilsMessengerCallbackDataEXT* pCallbackData,
+        void* pUserData) {
+
+        std::cerr << "validation layer: " << pCallbackData->pMessage << std::endl;
+
+        return VK_FALSE;
+    }
+
+    void populateDebugMessengerCreateInfo(VkDebugUtilsMessengerCreateInfoEXT& createInfo) {
+        createInfo = {};
+        createInfo.sType = VK_STRUCTURE_TYPE_DEBUG_UTILS_MESSENGER_CREATE_INFO_EXT;
+        createInfo.messageSeverity = VK_DEBUG_UTILS_MESSAGE_SEVERITY_VERBOSE_BIT_EXT | VK_DEBUG_UTILS_MESSAGE_SEVERITY_WARNING_BIT_EXT | VK_DEBUG_UTILS_MESSAGE_SEVERITY_ERROR_BIT_EXT;
+        createInfo.messageType = VK_DEBUG_UTILS_MESSAGE_TYPE_GENERAL_BIT_EXT | VK_DEBUG_UTILS_MESSAGE_TYPE_VALIDATION_BIT_EXT | VK_DEBUG_UTILS_MESSAGE_TYPE_PERFORMANCE_BIT_EXT;
+        createInfo.pfnUserCallback = debugCallback;
+    }
+    void setupDebugMessenger(VkDebugUtilsMessengerCreateInfoEXT &createInfo) {
+        createInfo.sType = VK_STRUCTURE_TYPE_DEBUG_UTILS_MESSENGER_CREATE_INFO_EXT;
+        createInfo.messageSeverity = VK_DEBUG_UTILS_MESSAGE_SEVERITY_VERBOSE_BIT_EXT | VK_DEBUG_UTILS_MESSAGE_SEVERITY_WARNING_BIT_EXT | VK_DEBUG_UTILS_MESSAGE_SEVERITY_ERROR_BIT_EXT;
+        createInfo.messageType = VK_DEBUG_UTILS_MESSAGE_TYPE_GENERAL_BIT_EXT | VK_DEBUG_UTILS_MESSAGE_TYPE_VALIDATION_BIT_EXT | VK_DEBUG_UTILS_MESSAGE_TYPE_PERFORMANCE_BIT_EXT;
+        createInfo.pfnUserCallback = debugCallback;
+        createInfo.pUserData = nullptr; // Optional
+    }
+
+    VkResult createDebugUtilsMessengerEXT(VkInstance instance, const VkDebugUtilsMessengerCreateInfoEXT* pCreateInfo,
+            const VkAllocationCallbacks* pAllocator, VkDebugUtilsMessengerEXT* pDebugMessenger) {
+        auto func = (PFN_vkCreateDebugUtilsMessengerEXT)vkGetInstanceProcAddr(instance, "vkCreateDebugUtilsMessengerEXT");
+        if (func != nullptr) {
+            return func(instance, pCreateInfo, pAllocator, pDebugMessenger);
+        }
+        else {
+            return VK_ERROR_EXTENSION_NOT_PRESENT;
+        }
     }
 
     bool RHI::init(SDL_Window *window) {
@@ -48,7 +106,8 @@ namespace segfault::renderer {
         if (result != VK_SUCCESS) {
             return false;
         }
-        
+        VkDebugUtilsMessengerCreateInfoEXT debugCreateInfo{};
+        setupDebugMessenger(debugCreateInfo);
 
         VkApplicationInfo appInfo{};
         createInstance(appInfo);
@@ -56,19 +115,38 @@ namespace segfault::renderer {
         uint32_t extensionCount = 0;
         const char** extensions;
         SDL_Vulkan_GetInstanceExtensions(window, &extensionCount, nullptr);
-        const char **extensionNames = new const char* [extensionCount];
-        SDL_Vulkan_GetInstanceExtensions(window, &extensionCount, extensionNames);
+        std::vector<const char*> extensionNames;
+        extensionNames.resize(extensionCount);
+        SDL_Vulkan_GetInstanceExtensions(window, &extensionCount, &extensionNames[0]);
 
-        const VkInstanceCreateInfo createInfo = {
-                VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO, // sType
-                nullptr,                                // pNext
-                0,                                      // flags
-                &appInfo,                                // pApplicationInfo
-                0,                                      // enabledLayerCount
-                nullptr,                                // ppEnabledLayerNames
-                extensionCount,                         // enabledExtensionCount
-                extensionNames,                         // ppEnabledExtensionNames
+        if (mImpl->enableValidationLayers) {
+            extensionNames.push_back(VK_EXT_DEBUG_UTILS_EXTENSION_NAME);
+        }
+
+        VkInstanceCreateInfo createInfo = {
+                VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO,         // sType
+                nullptr,                                        // pNext
+                0,                                              // flags
+                &appInfo,                                       // pApplicationInfo
+                0,                                              // enabledLayerCount
+                nullptr,                                        // ppEnabledLayerNames
+                static_cast<uint32_t>(extensionNames.size()),   // enabledExtensionCount
+                extensionNames.data()                           // ppEnabledExtensionNames
         };
+
+
+        VkDebugUtilsMessengerCreateInfoEXT debugCreateInfo{};
+        if (mImpl->enableValidationLayers && !checkValidationLayerSupport()) {
+            createInfo.enabledLayerCount = static_cast<uint32_t>(validationLayers.size());
+            createInfo.ppEnabledLayerNames = validationLayers.data();
+
+            populateDebugMessengerCreateInfo(debugCreateInfo);
+            createInfo.pNext = (VkDebugUtilsMessengerCreateInfoEXT*)&debugCreateInfo;
+        }
+        else {
+            createInfo.enabledLayerCount = 0;
+            createInfo.pNext = nullptr;
+        }
 
         result = vkCreateInstance(&createInfo, nullptr, &mImpl->instance);
         if (result != VK_SUCCESS) {
