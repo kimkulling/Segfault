8a2318b41ceb1c974bdc7390029e36edd8d7c9d7
Kim Kulling <kim.kulling@googlemail.com>
2025-11-26 08:49:41 +0100

    Fix formats

--- src/runtime/core/tokenizer.cpp
+++ src/runtime/core/tokenizer.cpp
@@ -2,69 +2,69 @@
 
 namespace segfault::core {
 
-using namespace ::cppcore;
+    using namespace ::cppcore;
 
-static const std::string EmptyToken = "";
+    static const std::string EmptyToken = "";
 
-size_t Tokenizer::getNumOfToken() const {
-    return m_Token.size();
-}
-
-const std::string &Tokenizer::getTokenAt( uint32_t index ) const {
-    if ( m_Token.isEmpty() || m_Token.size() >= index ) {
-        return EmptyToken;
+    size_t Tokenizer::getNumOfToken() const {
+        return m_Token.size();
     }
 
-    return m_Token[ index ];
-}
+    const std::string &Tokenizer::getTokenAt( uint32_t index ) const {
+        if ( m_Token.isEmpty() || m_Token.size() >= index ) {
+            return EmptyToken;
+        }
 
-bool Tokenizer::isEmpty() const {
-    return m_Token.isEmpty();
-}
+        return m_Token[ index ];
+    }
 
-void Tokenizer::clear() {
-    m_Token.clear();
-}
+    bool Tokenizer::isEmpty() const {
+        return m_Token.isEmpty();
+    }
 
-size_t Tokenizer::tokenize(const std::string &str, StringArray &tokens, const std::string &delimiters) {
-    // Skip delimiters at beginning.
-    std::string::size_type lastPos = str.find_first_not_of( delimiters, 0 );
-    
-    // find first "non-delimiter".
-    std::string::size_type pos = str.find_first_of( delimiters, lastPos );
-    while( std::string::npos != pos || std::string::npos != lastPos ) {
-        // Found a token, add it to the vector.
-        std::string tmp = str.substr( lastPos, pos - lastPos );
-        if ( !tmp.empty() && ' ' != tmp[ 0 ] ) {
-            tokens.add( tmp );
+    void Tokenizer::clear() {
+        m_Token.clear();
+    }
+
+    size_t Tokenizer::tokenize(const std::string &str, StringArray &tokens, const std::string &delimiters) {
+        // Skip delimiters at beginning.
+        std::string::size_type lastPos = str.find_first_not_of( delimiters, 0 );
+        
+        // find first "non-delimiter".
+        std::string::size_type pos = str.find_first_of( delimiters, lastPos );
+        while( std::string::npos != pos || std::string::npos != lastPos ) {
+            // Found a token, add it to the vector.
+            std::string tmp = str.substr( lastPos, pos - lastPos );
+            if ( !tmp.empty() && ' ' != tmp[ 0 ] ) {
+                tokens.add( tmp );
+            }
+            
+            // Skip delimiters.  Note the "not_of"
+            lastPos = str.find_first_not_of( delimiters, pos );
+            
+            // Find next "non-delimiter"
+            pos = str.find_first_of( delimiters, lastPos );
+        }
+
+        return static_cast<uint32_t>( tokens.size() );
+    }
+
+    size_t Tokenizer::tokenize( const std::string& str, const std::string& delimiters ) {
+        if( !m_Token.isEmpty() ) {
+            m_Token.resize( 0 );
         }
         
-        // Skip delimiters.  Note the "not_of"
-        lastPos = str.find_first_not_of( delimiters, pos );
+        tokenize( str, m_Token, delimiters );
         
-        // Find next "non-delimiter"
-        pos = str.find_first_of( delimiters, lastPos );
+        return m_Token.size();
     }
 
-    return static_cast<uint32_t>( tokens.size() );
-}
-
-size_t Tokenizer::tokenize( const std::string& str, const std::string& delimiters ) {
-    if( !m_Token.isEmpty() ) {
-        m_Token.resize( 0 );
+    bool Tokenizer::isLineBreak(char c) {
+        if ( c == '\n' ) {
+            return true;
+        } 
+        
+        return false;
     }
-    
-    tokenize( str, m_Token, delimiters );
-    
-    return m_Token.size();
-}
-
-bool Tokenizer::isLineBreak(char c) {
-    if ( c == '\n' ) {
-        return true;
-    } 
-    
-    return false;
-}
 
 } // namespace segfault::core
